b9960ae8dd40fb7b9eebb51e6a5b99208a411bc0
SamiJamal <42804026+SamiJamal@users.noreply.github.com>
2018-12-10 12:00:17 +0100

    Add files via upload

---
+++ blokjes.js
@@ -0,0 +1,4 @@
+document.getElementById('bl1').style.backgroundColor =  "deepskyblue";
+document.getElementById('bl2').style.backgroundColor =  "green";
+document.getElementById('bl3').style.backgroundColor =  "0, 255, 255";
+document.getElementById('bl4').style.backgroundColor =  "red";
---
+++ vulmetjs.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+document.getElementById('paginatitel').innerText = 'mijn geweldige pagina';
+document.getElementById('subkop').innerText = 'mijn geweldige subkop';
+document.getElementById('slagroomtaart').src = '../IMAGES/slagroomtaart.jpg';
+document.getElementById('slagroomtaart2.0').src = '../IMAGES/slagroomtaart2.0.jpg';
+
